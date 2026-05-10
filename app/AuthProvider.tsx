c56d27e424b8662469100e6c5e7b4479016a314c
Belal-Bari <tanvirj9@gmail.com>
2026-05-10 18:52:30 +0300

    Added authprovider and env variables for github OAuth

---
+++ app/AuthProvider.tsx
@@ -0,0 +1,12 @@
+'use-client'
+
+import { Session } from "inspector/promises";
+import { SessionProvider } from "next-auth/react"
+
+type Props = {
+    children: React.ReactNode;
+}
+
+export default function AuthProvider({ children }: Props) {
+    return <SessionProvider>{children}</SessionProvider>
+}
