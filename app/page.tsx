f9817fd0d471cb8027bdb5f4d3fa9d4d63a63e07
Belal-Bari <tanvirj9@gmail.com>
2026-05-12 17:01:27 +0300

    Added protected route

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect('/api/auth/signin');
+  }
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        
+        HomePage
       </main>
     </div>
   );
