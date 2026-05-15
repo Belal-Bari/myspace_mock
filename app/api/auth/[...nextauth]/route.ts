8af5ba3259e22cb60b951fc57def3db763238d94
Belal-Bari <tanvirj9@gmail.com>
2026-05-15 21:21:11 +0300

    Fixed Auth store

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,11 @@
+import { prisma } from '@/lib/prisma';
+import { PrismaAdapter } from '@next-auth/prisma-adapter' 
 import NextAuth from "next-auth";
 import type { NextAuthOptions } from "next-auth";
 import Github from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         Github({
             clientId: process.env.GITHUB_ID!,
